cb546acb5dde0307379b0be7c0b45067ab1f4338
Ramiro Melo <ramiromelo@gmail.com>
2021-01-08 00:03:05 -0800

    22. Step 5

--- 1. Blockchain Intuition/blockchain.py
+++ 1. Blockchain Intuition/blockchain.py
@@ -19,5 +19,15 @@ class Blockchain:
         self.chain = []
         self.create_block(proof = 1, previous_hash = '0')
         
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
+        self.chain.append(block)
+        return block
+    
+    def get_previous_block(self):
+        return self.chain[-1]
         
 # Part 2 - Mining a Blockchain
