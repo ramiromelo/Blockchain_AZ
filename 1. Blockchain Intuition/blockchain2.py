fef64d4d25e1ced219a5d6d50863aca21a071ff9
Ramiro Melo <ramiromelo@gmail.com>
2021-01-17 23:28:31 -0800

    34. Plan of Attack

--- 1. Blockchain Intuition/blockchain2.py
+++ 1. Blockchain Intuition/blockchain2.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan  7 21:39:23 2021
+# Module 1 - Create a Blockchain
 
-@author: RAMIRO
-"""
+# To be installed:
+# Flask==0.12.2: pip install Flask==0.12.2
+# Postman HTTP Client: https://www.getpostman.com/
 
-
+# Importing the libraries
 import datetime
 import hashlib
 import json
@@ -14,11 +13,11 @@ from flask import Flask, jsonify
 # Part 1 - Building a Blockchain
 
 class Blockchain:
-    
+
     def __init__(self):
         self.chain = []
         self.create_block(proof = 1, previous_hash = '0')
-        
+
     def create_block(self, proof, previous_hash):
         block = {'index': len(self.chain) + 1,
                  'timestamp': str(datetime.datetime.now()),
@@ -26,15 +25,15 @@ class Blockchain:
                  'previous_hash': previous_hash}
         self.chain.append(block)
         return block
-    
+
     def get_previous_block(self):
         return self.chain[-1]
-      
+
     def proof_of_work(self, previous_proof):
         new_proof = 1
         check_proof = False
         while check_proof is False:
-            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest() 
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
             if hash_operation[:4] == '0000':
                 check_proof = True
             else:
@@ -42,9 +41,9 @@ class Blockchain:
         return new_proof
     
     def hash(self, block):
-        encoded_block = jason.dumps(block, sort_keys = True).encode()
-        return hashlib.sha256(encoded_block).hexidigest()
-
+        encoded_block = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+    
     def is_chain_valid(self, chain):
         previous_block = chain[0]
         block_index = 1
@@ -61,23 +60,25 @@ class Blockchain:
             block_index += 1
         return True
 
-        
-# Part 2 - Mining a Blockchain
+# Part 2 - Mining our Blockchain
+
 # Creating a Web App
 app = Flask(__name__)
-        
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+
 # Creating a Blockchain
 blockchain = Blockchain()
 
 # Mining a new block
-@app.route('http://127.0.0.1:500/mine_block', methods = ['GET'])
+@app.route('/mine_block', methods = ['GET'])
 def mine_block():
     previous_block = blockchain.get_previous_block()
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
     block = blockchain.create_block(proof, previous_hash)
-    response = {'message': 'Congratulations, you just mined a block!', 
+    response = {'message': 'Congratulations, you just mined a block!',
                 'index': block['index'],
                 'timestamp': block['timestamp'],
                 'proof': block['proof'],
@@ -87,8 +88,19 @@ def mine_block():
 # Getting the full Blockchain
 @app.route('/get_chain', methods = ['GET'])
 def get_chain():
-    response = {'chain': blockchain.chain, 
+    response = {'chain': blockchain.chain,
                 'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+
+# Checking if the Blockchain is valid
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'All good. The Blockchain is valid.'}
+    else:
+        response = {'message': 'Houston, we have a problem. The Blockchain is not valid.'}
     return jsonify(response), 200
 
 # Running the app
